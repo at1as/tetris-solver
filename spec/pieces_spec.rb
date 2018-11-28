f8f4c4a7e0467b0f10ebf42e6c0024439891f76b
Jason Willems <jason@willems.ca>
2018-11-27 23:08:49 -0800

    Full piece rotation test coverage

--- spec/pieces_spec.rb
+++ spec/pieces_spec.rb
@@ -2,35 +2,64 @@ require_relative '../pieces'
 
 RSpec.describe Pieces do
 
-  it "should rotate 'L' piece and retain nils" do
+  it "should rotate 'J' piece and retain nils" do
     piece = [
-      ["l", nil, nil],
-      ["l", "l", "l"]
+      ["j", nil, nil],
+      ["j", "j", "j"]
     ]
 
     expect(Pieces.rotations(piece)).to eq(
       [
         [
-          ["l", nil, nil],
-          ["l", "l", "l"]
+          ["j", nil, nil],
+          ["j", "j", "j"]
+        ],
+        [
+          ["j", "j"],
+          ["j", nil],
+          ["j", nil]
+        ],
+        [
+          ["j", "j", "j"],
+          [nil, nil, "j"],
+        ],
+        [
+          [nil, "j"],
+          [nil, "j"],
+          ["j", "j"]
+        ]
+      ]
+    )
+  end
+
+  it "should rotate 'L' piece and retain nils" do
+    piece = [
+      ["l", "l", "l"],
+      ["l", nil, nil]
+    ]
+
+    expect(Pieces.rotations(piece)).to eq(
+      [
+        [
+          ["l", "l", "l"],
+          ["l", nil, nil]
         ],
         [
           ["l", "l"],
-          ["l", nil],
-          ["l", nil]
+          [nil, "l"],
+          [nil, "l"]
         ],
         [
-          ["l", "l", "l"],
           [nil, nil, "l"],
+          ["l", "l", "l"],
         ],
         [
-          [nil, "l"],
-          [nil, "l"],
+          ["l", nil],
+          ["l", nil],
           ["l", "l"]
         ]
       ]
     )
-
   end
 
   it "should rotate 'I' piece and remove duplicates" do
@@ -51,7 +80,6 @@ RSpec.describe Pieces do
         ]
       ]
     )
-
   end
 
   it "should rotate 'O' piece and remove duplicates" do
@@ -70,4 +98,75 @@ RSpec.describe Pieces do
     )
   end
 
+  it "should rotate 'S' piece" do
+    piece = [
+      [nil, "s", "s"],
+      ["s", "s", nil]
+    ]
+
+    expect(Pieces.rotations(piece)).to eq(
+      [
+        [
+          [nil, "s", "s"],
+          ["s", "s", nil]
+        ],
+        [
+          ["s", nil],
+          ["s", "s"],
+          [nil, "s"]
+        ],
+      ]
+    )
+  end
+
+  it "should rotate 'Z' piece" do
+    piece = [
+      ["s", "s", nil],
+      [nil, "s", "s"]
+    ]
+
+    expect(Pieces.rotations(piece)).to eq(
+      [
+        [
+          ["s", "s", nil],
+          [nil, "s", "s"]
+        ],
+        [
+          [nil, "s"],
+          ["s", "s"],
+          ["s", nil]
+        ],
+      ]
+    )
+  end
+
+  it "should rotate 'T' piece and retain nils" do
+    piece = [
+      [nil, "t", nil],
+      ["t", "t", "t"]
+    ]
+
+    expect(Pieces.rotations(piece)).to eq(
+      [
+        [
+          [nil, "t", nil],
+          ["t", "t", "t"]
+        ],
+        [
+          ["t", nil],
+          ["t", "t"],
+          ["t", nil]
+        ],
+        [
+          ["t", "t", "t"],
+          [nil, "t", nil],
+        ],
+        [
+          [nil, "t"],
+          ["t", "t"],
+          [nil, "t"]
+        ]
+      ]
+    )
+  end
 end
